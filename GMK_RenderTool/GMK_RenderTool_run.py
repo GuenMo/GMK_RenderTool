155a283dc44a1e93b8612c96d922c538c2e0a919
GuenMo Kim <hybrid0815@gmail.com>
2016-11-03 00:24:04 +0900

    aaa

--- GMK_RenderTool/GMK_RenderTool_run.py
+++ GMK_RenderTool/GMK_RenderTool_run.py
@@ -1,24 +1,17 @@
-# -*- coding:utf-8 -*-
-
-def GMK_RenderTool_run():
+def run():
     try:
         filePath = __file__
-        GMK_RenderTool_Path = filePath.rpartition("\\")[0]
+        appPath = filePath.rpartition("\\")[0]
     except:
-        print "Environ Value 'GMK_RenderTool_Path' not exist."
-    
+        print "Application`s path not exist."
+
     else:
         import sys
-        path = GMK_RenderTool_Path
-        
+        path = appPath
+
         if not path in sys.path:
             sys.path.append(path)
-        
-        import UI_RenderTool.GMK_RnederTool_MainWindow as mainWindow
-        reload(mainWindow)
-        
-        mainWindow.main()
-    
-if __name__ == "GMK_RenderTool_run":    
-    GMK_RenderTool_run()    
-    
+
+        import UI_RenderTool.GMK_RnederTool_MainWindow 
+        reload(UI_RenderTool.GMK_RnederTool_MainWindow )
+        UI_RenderTool.GMK_RnederTool_MainWindow 
